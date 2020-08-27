d94781d037bb873e7fd4589a6c1300026de4177a
idolusky <idoluski@gmail.com>
2020-08-27 20:32:18 +0300

    changed the ajax method

--- main.js
+++ main.js
@@ -37,15 +37,27 @@ $.ajax({
     url: 'https://randomuser.me/api/?results=10',
     dataType: 'json',
     success: function (data) {
-        for (i = 0; i < 10; i++) {
-            const users = [{
-                id: data.results[i].login.salt
-                , email: data.results[i].email
-                , name: { first: data.results[i].name.first, last: data.results[i].name.last }
-            }]
-            console.log(data);
-            renderPeople(users);
-        }
+        //  for (i = 0; i < 10; i++) {
+        // const users = [{
+        //     id: data.results[i].login.salt
+        //     , email: data.results[i].email
+        //     , name: { first: data.results[i].name.first, last: data.results[i].name.last }
+        // }]
+
+        const users = data.results.map(u => {
+            return {
+                id: u.login.salt,
+                email: u.email,
+                name: {
+                    first: u.name.first,
+                    last: u.name.last
+                }
+            }
+        })
+
+        console.log(data);
+        renderPeople(users);
+        // }
     }
 });
 
@@ -56,4 +68,3 @@ const renderPeople = function (users) {
     const newHTML = template({ users })
     $('#people').append(newHTML);
 }
-renderPeople()
