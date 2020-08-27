7bbe9af71f56e164d1b075bc3f1588d8d3a4381d
idolusky <idoluski@gmail.com>
2020-08-27 13:47:16 +0300

    fixed the ajax call

--- main.js
+++ main.js
@@ -33,61 +33,27 @@
 //         console.log(data);
 //     }
 // });
+$.ajax({
+    url: 'https://randomuser.me/api/?results=10',
+    dataType: 'json',
+    success: function (data) {
+        for (i = 0; i < 10; i++) {
+            const users = [{
+                id: data.results[i].login.salt
+                , email: data.results[i].email
+                , name: { first: data.results[i].name.first, last: data.results[i].name.last }
+            }]
+            console.log(data);
+            renderPeople(users);
+        }
+    }
+});
 
-let users = [{
-    Id: 1,
-    email: "gary.harper@example.com",
-    name: { title: "Mr", first: "Gary", last: "Harper" }
-}, {
-    Id: 2,
-    email: "katrin.rolland@example.com",
-    name: { title: "Madame", first: "Katrin", last: "Rolland" }
-}, {
-    Id: 3,
-    email: "clara.cortes@example.com",
-    name: { title: "Mrs", first: "Clara", last: "Cortes" }
-},
-{
-    Id: 4,
-    email: "songul.platje@example.com",
-    name: { title: "Mrs", first: "Songül", last: "Platje" }
-},
-{
-    Id: 5,
-    email: "julian.sanz@example.com",
-    name: { title: "Mr", first: "Julian", last: "Sanz" }
-},
-{
-    Id: 6,
-    email: "minea.hamalainen@example.com",
-    name: { title: "Ms", first: "Minea", last: "Hamalainen" }
-},
-{
-    Id: 7,
-    email: "tirill.eidem@example.com",
-    name: { title: "Mrs", first: "Tirill", last: "Eidem" }
-},
-{
-    Id: 8,
-    email: "morris.nelson@example.com",
-    name: { title: "Mr", first: "Morris", last: "Nelson" }
-},
-{
-    Id: 9,
-    email: "alan.graves@example.com",
-    name: { title: "Mr", first: "Alan", last: "Graves" }
-},
-{
-    Id: 10,
-    email: "owen.ennis@example.com",
-    name: { title: "Mr", first: "Owen", last: "Ennis" }
-}
-]
 
-const renderPeople = function () {
+const renderPeople = function (users) {
     const source = $("#users-template").html()
     const template = Handlebars.compile(source)
-    const newHTML = template({ users: users })
+    const newHTML = template({ users })
     $('#people').append(newHTML);
 }
 renderPeople()
